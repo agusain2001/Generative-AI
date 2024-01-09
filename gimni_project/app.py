babe995b3d800d549cae42a8b4bcad0b5e4debb6
Ashish Gusain <agusain2001@gmail.com>
2024-01-09 20:29:58 +0530

    pushing project into github

---
+++ gimni_project/app.py
@@ -0,0 +1,53 @@
+from dotenv import load_dotenv
+load_dotenv()  # Load all the environment variables from .env
+
+import streamlit as st
+import os
+from PIL import Image
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model = genai.GenerativeModel('gemini-pro-vision')
+
+def get_gemini_response(input, image, prompt):
+    response = model.generate_content([input, image[0], prompt])
+    return response.text
+
+def input_image_details(uploaded_file):
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+# Set page configuration
+st.set_page_config(page_title="MultiLanguage Invoice Extractor")
+
+st.header("MultiLanguage Invoice Extractor")
+input_prompt = """
+You are an expert in understanding invoices. We will upload an image as an invoice, and you 
+will have to answer any question based on the uploaded invoice image.
+"""
+
+input_text = st.text_input("Input Prompt: ", key="input")
+upload_file = st.file_uploader("Choose an image...", type=['jpg', 'jpeg', 'png'])
+image = ""
+
+if upload_file is not None:
+    image = Image.open(upload_file)
+    st.image(image, caption="Upload Image...", use_column_width=True)
+
+submit = st.button("Tell me about the invoice ")
+
+if submit:
+    image_data = input_image_details(upload_file)
+    response = get_gemini_response(input_text, image_data, input_prompt)
+    st.subheader("The Response is")
+    st.write(response)
